d1209dcef7b88f5275bc4f3c4ad07af8cdc19aa4
Lukman Hidayah <lukmanhidayah01@gmail.com>
2021-04-28 10:39:56 +0700

    Update package

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lukman Hidayah
         </a>
       </header>
     </div>
